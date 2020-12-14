cf34b6523fa55d62b3e8da6c0e7c8f2674d49d06
David <david.nir@intel.com>
2020-12-14 11:22:24 +0200

    p1,2

--- Assets/Scripts/CatmullClark.cs
+++ Assets/Scripts/CatmullClark.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 
 public class CCMeshData
@@ -41,13 +42,61 @@ public static class CatmullClark
     // f1, f2 are faces incident to the edge. If the edge belongs to one face only, f2 is -1
     public static List<Vector4> GetEdges(CCMeshData mesh)
     {
-        return null;
+        //List<Vector4> outList = new List<Vector4>();
+        HashSet<Vector4> outList = new HashSet<Vector4>();
+
+        for (int i=0; i<mesh.faces.Count(); i++)
+        {
+            Vector4 face = mesh.faces[i];
+            float[] verts = new float[4] { face.x, face.y, face.z, face.w };
+
+            List<Vector2> faceEdges = new List<Vector2>();
+            for (int f=0; f<verts.Length; f++)
+            {
+                float a = face[f];
+                float b = face[(f + 1) % 4];
+                faceEdges.Add(new Vector2(Math.Min(a, b), Math.Max(a, b)));
+            }
+            HashSet<Vector2> sharedEdges = new HashSet<Vector2>();
+            // add all adjacent faces to the out vector list
+            for (int j = 0; j < mesh.faces.Count(); j++)
+            {
+                Vector4 otherFace = mesh.faces[j];
+                float[] otherVerts = new float[4] { otherFace.x, otherFace.y, otherFace.z, otherFace.w };
+                var intersection = verts.Intersect(otherVerts).ToArray();
+                
+                if (intersection.Count() == 2)
+                {
+                    float a = intersection[0];
+                    float b = intersection[1];
+                    sharedEdges.Add(new Vector2(Math.Min(a, b), Math.Max(a, b)));
+                    Vector4 edgeVector = new Vector4(intersection[0], intersection[1], Math.Max(i, j), Math.Min(i, j));
+                    outList.Add(edgeVector);
+                }
+            }
+            // here we check if the current face has less than 4 adjacent faces (and if so we add a -1 index as otherFace)
+            foreach(Vector2 edge in faceEdges)
+            {
+                if (! sharedEdges.Contains(edge))
+                {
+                    outList.Add(new Vector4(edge.x, edge.y, i, -1));
+                }
+            }
+        }
+        return outList.ToList();
     }
 
     // Returns a list of "face points" for the given CCMeshData, as described in the Catmull-Clark algorithm 
     public static List<Vector3> GetFacePoints(CCMeshData mesh)
     {
-        return null;
+        List<Vector3> outList = new List<Vector3>();
+        foreach (Vector4 face in mesh.faces)
+        {
+            List<float> pointIndices = new float[4] { face.x, face.y, face.z, face.w }.ToList();
+            List<Vector3> subList = mesh.points.Where((item, index) => pointIndices.Contains(index)).ToList();
+            var average = subList.Aggregate(new Vector3(0, 0, 0), (s, v) => s + v) / (float)subList.Count;
+        }
+        return outList;
     }
 
     // Returns a list of "edge points" for the given CCMeshData, as described in the Catmull-Clark algorithm 
