59b89c534c8a37e7cdf97963ed988cd2f1e99b98
Tamar Yovell <tamar.yov@cs.huji.ac.il>
2020-12-14 15:05:12 +0200

    Update CatmullClark.cs

--- Assets/Scripts/CatmullClark.cs
+++ Assets/Scripts/CatmullClark.cs
@@ -15,6 +15,7 @@ public class CCMeshData
 }
 
 
+
 public static class CatmullClark
 {
     // Returns a QuadMeshData representing the input mesh after one iteration of Catmull-Clark subdivision.
@@ -31,9 +32,82 @@ public static class CatmullClark
 
         // Combine facePoints, edgePoints and newPoints into a subdivided QuadMeshData
 
-        // Your implementation here...
+        // We create a list with length meshData.points.Count() which maps each vertex to its neighbor edges
+        List<List<int>> edgesByVertex = new List<List<int>>();
+        for (int i = 0; i < meshData.points.Count(); i++)
+        {
+            edgesByVertex.Add(new List<int>());
+        }
 
-        return new QuadMeshData();
+        for (int i = 0; i < meshData.edges.Count(); i++)
+        {
+            Vector4 edge = meshData.edges[i];
+            edgesByVertex[(int) edge.x].Add(i);
+            edgesByVertex[(int) edge.y].Add(i);
+        }
+
+
+        List<Vector3> newVertices = new List<Vector3>(meshData.newPoints);
+        Dictionary<Vector3, int> edgePointIndices = new Dictionary<Vector3, int>();
+        int newPointIndex = newVertices.Count();
+        var newQuads = new List<Vector4>();
+        for (int i = 0; i < meshData.faces.Count(); i++)
+        {
+            Vector3 c = meshData.facePoints[i];
+            newVertices.Add(c);
+            int c_index = newPointIndex++;
+            for (int j = 0; j < 4; j++)
+            {
+
+                int a = (int) meshData.faces[i][j];
+                Debug.Log((j + 1) % 4);
+                Debug.Log((j + 3) % 4);
+                Debug.Log("---");
+
+                int a_next = (int) meshData.faces[i][(j + 1) % 4];
+                int a_prev = (int) meshData.faces[i][(j + 3) % 4];
+
+                Vector3 b = Vector4.zero;
+                Vector3 d = Vector4.zero;
+                int b_index = -1;
+                int d_index = -1;
+                foreach (int edgeIndex in edgesByVertex[a])
+                {
+                    Vector4 edge = meshData.edges[edgeIndex];
+                    if (a_next == edge.x || a_next == edge.y)
+                    {
+                        b = meshData.edgePoints[edgeIndex];
+                        if (!edgePointIndices.ContainsKey(b))
+                        {
+                            newVertices.Add(b);
+                            b_index = newPointIndex;
+                            edgePointIndices.Add(b, newPointIndex++);
+                        }
+                        else
+                        {
+                            b_index = edgePointIndices[b];
+                        }
+                    }
+                    if (a_prev == edge.x || a_prev == edge.y)
+                    {
+                        d = meshData.edgePoints[edgeIndex];
+                        if (!edgePointIndices.ContainsKey(d))
+                        {
+                            newVertices.Add(d);
+                            d_index = newPointIndex;
+                            edgePointIndices.Add(d, newPointIndex++);
+                        }
+                        else
+                        {
+                            d_index = edgePointIndices[d];
+                        }
+                    }
+                }
+                Vector4 newFace = new Vector4(a, b_index, c_index, d_index);
+                newQuads.Add(newFace);
+            }
+        }
+        return new QuadMeshData(newVertices, newQuads);
     }
 
     // Returns a list of all edges in the mesh defined by given points and faces.
@@ -95,6 +169,7 @@ public static class CatmullClark
             List<float> pointIndices = new float[4] { face.x, face.y, face.z, face.w }.ToList();
             List<Vector3> subList = mesh.points.Where((item, index) => pointIndices.Contains(index)).ToList();
             var average = subList.Aggregate(new Vector3(0, 0, 0), (s, v) => s + v) / (float)subList.Count;
+            outList.Add(average);
         }
         return outList;
     }
@@ -102,12 +177,94 @@ public static class CatmullClark
     // Returns a list of "edge points" for the given CCMeshData, as described in the Catmull-Clark algorithm 
     public static List<Vector3> GetEdgePoints(CCMeshData mesh)
     {
-        return null;
+        List<Vector3> outList = new List<Vector3>();
+        foreach (Vector4 edge in mesh.edges)
+        {
+            //Vector4(p1, p2, f1, f2)
+            var p1 = mesh.points[(int) edge[0]];
+            var p2 = mesh.points[(int) edge[1]];
+            var f1 = mesh.facePoints[(int) edge[2]];
+            Vector4 newEdgePoint = Vector4.zero;
+            if (edge[3] != -1)
+            {
+                var f2 = mesh.facePoints[(int) edge[3]];
+                newEdgePoint = (p1 + p2 + f1 + f2) / 4;
+            }
+            else
+            {
+                var f2 = Vector4.zero;
+                newEdgePoint = (p1 + p2 + f1) / 3;
+            }
+
+            outList.Add(newEdgePoint);
+        }   
+        return outList;
     }
+
+
+    public struct NewPointData
+    {
+        public Vector3 coordinates;
+        public List<int> edges;
+        public List<int> faces;
+    }
+
 
     // Returns a list of new locations of the original points for the given CCMeshData, as described in the CC algorithm 
     public static List<Vector3> GetNewPoints(CCMeshData mesh)
     {
-        return null;
+        List<Vector3> outList = new List<Vector3>();
+        List<NewPointData> newPointsData = new List<NewPointData>();
+        
+        //Populating newPointsData
+        foreach (Vector3 point in mesh.points)
+        {
+            NewPointData data = new NewPointData() {coordinates=point, edges = new List<int>(), faces = new List<int>()};
+            newPointsData.Add(data);
+        }
+        for (int i=0; i<mesh.edges.Count(); i++)
+        {
+            Vector4 edge = mesh.edges[i];
+            int p1 = (int) edge[0];
+            int p2 = (int) edge[1];
+            int f1 = (int) edge[2];
+            int f2 = (int) edge[3];
+
+            newPointsData[p1].faces.Add(f1);
+            newPointsData[p2].faces.Add(f1);
+
+            if (f2 != -1)
+            {
+                newPointsData[p1].faces.Add(f2);
+                newPointsData[p2].faces.Add(f2);
+            }
+
+            newPointsData[p1].edges.Add(i);
+            newPointsData[p2].edges.Add(i);
+        }
+
+        foreach (NewPointData newPointData in newPointsData)
+        {
+            Vector3 f = Vector3.zero;
+            foreach(int faceIndex in newPointData.faces)
+            {
+                f += mesh.facePoints[faceIndex];
+            }
+            f /= newPointData.faces.Count();
+
+            Vector3 r = Vector3.zero;
+            foreach(int edgeIndex in newPointData.edges)
+            {
+                Vector4 edge = mesh.edges[edgeIndex]; //Vector4(p1, p2, f1, f2)
+                r += mesh.points[(int) edge[0]] + mesh.points[(int) edge[1]];
+            }
+            int n = newPointData.edges.Count();
+            r /= 2 * n;
+
+            Vector3 newPointLocation = (f + (2 * r) + ((n - 3) * newPointData.coordinates)) / n;
+            outList.Add(newPointLocation);
+        }
+
+        return outList;
     }
 }
